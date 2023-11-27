bac829b68d95b166171dfbb358ffa3b6144c695a
amanprajapat08278 <amanprajapat08278@gmail.com>
2023-11-27 13:56:44 +0530

    database and port added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DATABASE_URL } from './modules/common/provider/database.providers';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './modules/common/provider/database.providers';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT || 4004);
+  console.log('starting on port' + (PORT || 4004));
 }
 bootstrap();
